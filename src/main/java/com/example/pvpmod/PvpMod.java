3e56eefc1751726169cbc74b6d73eb91bb85cd00
jujicorparation-create <jujicorparation@gmail.com>
2026-07-09 02:53:35 +0500

    Update PvpMod.java

--- src/main/java/com/example/pvpmod/PvpMod.java
+++ src/main/java/com/example/pvpmod/PvpMod.java
@@ -2,18 +2,20 @@ package com.example.pvpmod;
 
 import com.example.pvpmod.module.ModuleManager;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.Entity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PvpMod implements ModInitializer {
     public static final String MOD_ID = "pvpmod";
     public static final Logger LOGGER = LoggerFactory.LoggerFactory.getLogger(MOD_ID);
+    
+    // Oxirgi urilgan dushmanni saqlash uchun universal o'zgaruvchi
+    public static Entity currentTarget = null;
 
     @Override
     public void onInitialize() {
         LOGGER.info("PvP Mod asosiy qismi yuklanmoqda...");
-        // Modullarni xotiraga yuklash
         ModuleManager.init();
     }
 }
-
