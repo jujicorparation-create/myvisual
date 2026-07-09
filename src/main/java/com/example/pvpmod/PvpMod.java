cc2e1f38f62293079ed53dad2d965acf2c4c539c
jujicorparation-create <jujicorparation@gmail.com>
2026-07-09 12:37:09 +0500

    Update PvpMod.java

--- src/main/java/com/example/pvpmod/PvpMod.java
+++ src/main/java/com/example/pvpmod/PvpMod.java
@@ -1,21 +1,21 @@
 package com.example.pvpmod;
 
-import com.example.pvpmod.client.ModKeybindings;
 import com.example.pvpmod.module.ModuleManager;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.Entity; // Import qo'shildi
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory; // Import qo'shildi!
+import org.slf4j.LoggerFactory;
 
 public class PvpMod implements ModInitializer {
     public static final String MOD_ID = "pvpmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    // Mixinlar qidirayotgan currentTarget o'zgaruvchisi qo'shildi!
+    public static Entity currentTarget = null; 
+
     @Override
     public void onInitialize() {
         LOGGER.info("Purple PvP Client yuklanmoqda...");
-        
-        // Modullarni va tugmalarni ishga tushirish
         ModuleManager.init();
-        ModKeybindings.init();
     }
 }
